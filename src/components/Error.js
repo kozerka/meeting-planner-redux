6c6499c7a1bb69cc062fb9afb809ce381d5b4e03
kozerka <kozerka@gmail.com>
2023-10-14 13:12:36 +0200

    Add Loader and Error components

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { BiSolidErrorAlt } from 'react-icons/bi';
+import FlexContainer from './ui/FlexContainer.styled';
+import styled from 'styled-components';
 
+const ErrorWrapper = styled(FlexContainer)`
+	align-items: center;
+	flex-direction: column;
+`;
+const StyledP = styled.p`
+	margin-top: 2rem;
+	font-size: ${({ theme }) => theme.fontSize.medium};
+	color: ${({ theme }) => theme.colors.text};
+`;
 const Error = () => {
-	return <div>Error</div>;
+	return (
+		<ErrorWrapper>
+			<BiSolidErrorAlt size="4rem" color="#fff" />
+			<StyledP>Something went wrong while fetching data...</StyledP>
+		</ErrorWrapper>
+	);
 };
 
 export default Error;
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Hourglass } from 'react-loader-spinner';
+import FlexContainer from './ui/FlexContainer.styled';
+import styled from 'styled-components';
+
+const LoaderWrapper = styled(FlexContainer)`
+	align-items: center;
+	flex-direction: column;
+`;
+const StyledP = styled.p`
+	margin-top: 2rem;
+	font-size: ${({ theme }) => theme.fontSize.medium};
+	color: ${({ theme }) => theme.colors.text};
+`;
 
 const Loader = () => {
-	return <div>Loader</div>;
+	return (
+		<LoaderWrapper>
+			<Hourglass
+				visible={true}
+				height="80"
+				width="80"
+				ariaLabel="hourglass-loading"
+				wrapperStyle={{}}
+				wrapperClass=""
+				colors={['#ddd', '#ffffff']}
+			/>
+			<StyledP>Loading meetings from API...</StyledP>
+		</LoaderWrapper>
+	);
 };
 
 export default Loader;
